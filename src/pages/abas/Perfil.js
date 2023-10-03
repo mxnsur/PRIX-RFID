acfb4b694849b76d42112678b13966894be1f07a
Rafael Mansur <rafaelmansursn@gmail.com>
2023-10-03 02:04:13 -0300

    criação botão ver UID

--- src/pages/abas/Perfil.js
+++ src/pages/abas/Perfil.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { ScrollView, Image, StyleSheet, Text } from 'react-native';
+import { ScrollView, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import Collapsible from 'react-native-collapsible';
 
 const Perfil = () => {
+
+  const [collapsed, setCollapsed] = React.useState(true);
+  const toggleExpand=()=>{
+    setCollapsed(!collapsed)
+  }
   return (
     <ScrollView style={styles.container}>
       <Image style={styles.logo} source={require('../../img/prix.png')} />
@@ -15,6 +21,13 @@ const Perfil = () => {
         accessible={true}
         accessibilityLabel={'Imagem Perfil'}
       />
+      
+      <TouchableOpacity onPress={toggleExpand} style={styles.button} >
+       <Text style={styles.buttonText}>Ver sua UID</Text>
+      </TouchableOpacity>
+      <Collapsible collapsed={collapsed}>
+        <Text style={styles.headingSection}>UID da TAG: ED 78 03 CA</Text>
+      </Collapsible>
     </ScrollView>
   );
 };
@@ -24,6 +37,14 @@ const styles = StyleSheet.create({
     flex: 1, 
     backgroundColor: "#fafafa"
     }, 
+
+  headingSection: { 
+    marginTop: 2,
+    color: '#333333',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+   }, 
 
   logo: {
     marginTop: 20,
@@ -49,6 +70,26 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-});
+
+  button: {
+    padding: 5,
+    marginTop: 20,
+    marginLeft: 30, 
+    marginRight: 30, 
+    backgroundColor: '#171717',
+    borderColor: '#171717',
+    borderWidth: 6,
+    borderRadius: 90,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+
+    buttonText: {
+      fontSize: 22,
+      color: 'white',
+    },
+     
+},
+);
 
 export default Perfil;
